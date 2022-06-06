30c0e6894424af7707c2f9945c7be05cc2fcc2b6
Gabriel Andres Melgar Aragón <88795540+gmelgar2020099@users.noreply.github.com>
2022-06-06 09:08:38 -0600

    cambio

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/Ligas', {useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log("Se encuentra conectado a la base de datos");
 
-    app.listen(3000, function(){
+    app.listen(process.env.PORT || 3000, function(){
         console.log("Esta corriendo en el puerto 3000")
     })
 
